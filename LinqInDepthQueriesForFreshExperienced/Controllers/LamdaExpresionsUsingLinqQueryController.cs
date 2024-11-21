77f4f57e5dc3620c333f4c0fa7a2577aa24c5e7f
Dell <Dell@DESKTOP-AAO14OC>
2024-11-21 07:03:59 +0530

    Userstory_123456:Created Lamdaexpression queries

---
+++ LinqInDepthQueriesForFreshExperienced/Controllers/LamdaExpresionsUsingLinqQueryController.cs
@@ -0,0 +1,132 @@
+﻿using LinqInDepthQueriesForFreshExperienced.Models;
+using LinqInDepthQueriesForFreshExperienced.NorthWind_Connect;
+using LinqInDepthQueriesForFreshExperienced.NorthWind_DB_DBConnect;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LinqInDepthQueriesForFreshExperienced.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LamdaExpresionsUsingLinqQueryController : ControllerBase
+    {
+        NorthwindDbContext _northwind_DBContext;
+        NorthwindContext _northwindContext;
+
+        public LamdaExpresionsUsingLinqQueryController(NorthwindDbContext northwind_DBContext, NorthwindContext northwindContext)
+        {
+            _northwind_DBContext = northwind_DBContext;
+            _northwindContext = northwindContext;
+        }
+        [HttpGet]
+        [Route("GetEmployeesData")]
+        //2nd of shortcutfor routing 
+        //[HttpGet("GetEmployeesData")]
+        //Example: Fetching All Records from employee table example
+        public async Task<IActionResult> GetEmployeesData()
+        {
+            //Basic LamdaLinQ synatx is
+            //A lambda expression is written using the => lamda operator
+            //lamda expressions will reduce the normal linq query synatx.
+            //now a days in realtime we are using this lamda expressions with linq.
+
+            //Normal LinqQuery:  var result = from abc in _northwind_DBContext.Employees select abc;
+            //Lamda expression Linq queryis below for fetching data from employee
+            var result = _northwind_DBContext.Employees.ToList(); // Returns all employees data with all columns.
+
+            //sqlqueryconverted by compiler:select * from employees
+            //the below written for json serialization refrence looping purpose written .net 8.0 to fix this refrence looping we are using this one.lower versions you will not get.
+            var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            //It converts your data to jsonformat
+            var convertedData = JsonConvert.SerializeObject(result, jsonSettings);
+            return StatusCode(StatusCodes.Status200OK, convertedData);
+
+        }
+
+        [HttpGet]
+        [Route("GetEmployeesDatawith_CityWise")]
+        public async Task<IActionResult> GetAllEmployeesDataCityWise()
+        {
+
+            //Normal LINQ QUERY:var result = from a in _northwind_DBContext.Employees where a.City == "London" select a;//linqquey 
+            //SqlQuery:     //select * from  Employees where City='London'
+            //LAMDA EXPRESSION USING LINQ query:
+            var result = _northwind_DBContext.Employees.Where(a => a.City == "London").ToList();//=>we called lamda opertor
+                                                 //(parameters) => expression
+            //here expression is a anoymous function.these functions we used in lamda expressions
+            var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            //It converts your data to jsonformat
+            var convertedData = JsonConvert.SerializeObject(result);
+            return StatusCode(StatusCodes.Status200OK, convertedData);
+         // var highEarners = context.Employees.Where(e => e.Salary > 50000).ToList(); 
+           // Filters employees with salary > 50,000
+        }
+        [HttpGet]
+        [Route("GetEmployeesDatawith_ReuiredColumnsonlyShowing")]
+        public async Task<IActionResult> GetEmployeesDatawith_ReuiredColumnsonlyShowing()
+        {//here we are fetchingall the data and showing only one column only.
+         //normal linq query:var result = from a in _northwind_DBContext.Employees select new { EmployeeFullName = a.FirstName + a.LastName };
+         //SqlQuery Format:select FirstName+LastName as 'EmployeeFullName' from  Employees 
+         //Lamda Expression With Linq query:
+            var result = _northwind_DBContext.Employees.Select(e => new { e.FirstName, e.LastName,e.Address,e.City}).ToList();
+            var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            //It converts your data to jsonformat
+            var convertedData = JsonConvert.SerializeObject(result);
+            return StatusCode(StatusCodes.Status200OK, convertedData);
+
+        }
+        [HttpGet]
+        [Route("GetOrderDataWithNamestatswiths")]
+        public async Task<IActionResult> GetDataByNamesStartswiths()
+        {//here we are fetchingall employess  data.
+         //normal linq query:var result = from s in _northwind_DBContext.Customers where s.ContactName.StartsWith("A") select s;
+         //lamda expression linq query like below.
+            var result = _northwind_DBContext.Customers.Where(a => a.ContactName.StartsWith("A")).ToList();
+                       //SQLQUERY:select * from Customers where ContactName like 'A%'
+                       var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            //It converts your data to jsonformat
+            var convertedData = JsonConvert.SerializeObject(result);
+            return StatusCode(StatusCodes.Status200OK, convertedData);
+
+        }
+
+        [HttpGet]
+        [Route("OrderByusage")]
+        public async Task<IActionResult> OrderbyUsage()
+        {
+            /*
+             //Normal linq queries with ascending order and descending order write like this.
+               var orderByAscendingResult = from s in lststudentsObj
+                                            orderby s.StudentName ascending
+                                            select s;//it will Show the  total columns 
+                                                     //Select new{StudentId,Age}//you can also select required columns
+               var orderByDescendingResult = from s in lststudentsObj
+                                             orderby s.StudentName descending
+                                             select s;
+            */
+            //ascending order/descending order  lamda expresion linq query.
+            var orderByAscendingResult = _northwind_DBContext.Customers.OrderBy(e=>e.ContactName).ToList();//ascending order
+            var orderByDescendingResult = _northwind_DBContext.Customers.OrderByDescending(e => e.ContactName).ToList();//descending order
+
+            //It converts your data to jsonformat
+            var convertedData = JsonConvert.SerializeObject(orderByDescendingResult);
+            return StatusCode(StatusCodes.Status200OK, convertedData);
+
+        }
+        [HttpGet]
+        [Route("GroupByusageWithOrginalTable")]
+        public async Task<IActionResult> GroupByusage()
+        {
+           
+            //Sql Groupby Query:select   CompanyName,Count(*)     from Customers group by CompanyName
+            var groupedCompanyNameData = _northwind_DBContext.Customers.GroupBy(s => s.CompanyName)
+                                     .Select(g => new { CompanyName = g.Key, CompanyName1 = g.ToList() });
+            //It converts your data to jsonformat
+            var convertedData = JsonConvert.SerializeObject(groupedCompanyNameData);
+            return StatusCode(StatusCodes.Status200OK, convertedData);
+
+        }
+    }
+    
+}
--- LinqInDepthQueriesForFreshExperienced/Controllers/NormalLinqQueriesController.cs
+++ LinqInDepthQueriesForFreshExperienced/Controllers/NormalLinqQueriesController.cs
@@ -13,7 +13,7 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
     public class NormalLinqQueriesController : ControllerBase
     {
         NorthwindDbContext _northwind_DBContext;
-     NorthwindContext _northwindContext;
+        NorthwindContext _northwindContext;
 
         public NormalLinqQueriesController(NorthwindDbContext northwind_DBContext, NorthwindContext northwindContext)
         {
@@ -32,7 +32,7 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
             //here (optional clause ) means where clause,order by clause,Group by Clause...
 
             //here we are fetchingall employess  data.
-        
+            //var means implcit typed local variable
             //synatx://var result=from localvariablename in datasource  (optional clause ) select localvariablename
             //Note:abc means its a localvariablename
             var result = from abc in _northwind_DBContext.Employees select abc;
@@ -48,23 +48,23 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
         [Route("GetEmployeesDatawith_CityWise")]
         public async Task<IActionResult> GetAllEmployeesDataCityWise()
         {
-  //synatx://var result=from localvariablename in datasource  (optional clause ) select localvariablename   
+            //synatx://var result=from localvariablename in datasource  (optional clause ) select localvariablename   
             //it will return employee data with it department along with all the columns data
-     var result = from a in _northwind_DBContext.Employees where a.City == "London" select a;//linqquey 
-  //SqlQuery:     //select * from  Employees where City='London'
+            var result = from a in _northwind_DBContext.Employees where a.City == "London" select a;//linqquey 
+                                                                                                    //SqlQuery:     //select * from  Employees where City='London'
 
 
-        var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
             //It converts your data to jsonformat
             var convertedData = JsonConvert.SerializeObject(result);
             return StatusCode(StatusCodes.Status200OK, convertedData);
 
         }
         [HttpGet]
-        [Route("GetEmployeesDatawith_CityWiseonlyname")]
-        public async Task<IActionResult> GetrequiredNamesCityWise()
+        [Route("GetEmployeesDatawith_ReuiredColumnsonlyShowing")]
+        public async Task<IActionResult> GetEmployeesDatawith_ReuiredColumnsonlyShowing()
         {//here we are fetchingall the data and showing only one column only.
-            var result = from a in _northwind_DBContext.Employees select new { EmployeeFullName = a.FirstName+a.LastName };
+            var result = from a in _northwind_DBContext.Employees select new { EmployeeFullName = a.FirstName + a.LastName };
             //SqlQuery Format:select FirstName+LastName as 'EmployeeFullName' from  Employees 
 
             var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
@@ -90,7 +90,7 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
         public async Task<IActionResult> GetDataByUsingJoins()
         {
             //here we are fetching employess&DepartMenent with data by using joins and orderby descending with required columns.
-            //sqlquery:select e.FirstName, e.LastName, e.City, d.DeptName from employee e join Departments d on d.Id=e.EmpId order by e.City desc
+            //sqlquery:select e.FirstName, e.LastName, e.City, d.DeptName from employee e join Departments d on d.Id==e.EmpId order by e.City desc
             var result = from e in _northwindContext.Employees join d in _northwindContext.Departments on e.EmpId equals d.Id orderby e.City descending select new { e.FirstName, e.LastName, e.City, d.DeptName };
             var jsonSettings = new JsonSerializerSettings { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
             //It converts your data to jsonformat
@@ -117,7 +117,16 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
             //if you want to get the only first 5 records in a table use this take(number) method.
             //after using the take() method you can use skip() method .
             //skip will skip or ignore the given count of records after taking the records.
-            //select top 5 * from customers
+
+            /*
+             * WITH CTE AS (
+            SELECT *, ROW_NUMBER() OVER (ORDER BY CustomerID) AS rn
+            FROM customers
+            )
+            SELECT * FROM CTE WHERE rn = 1;
+            */
+            //BASED ON YOU REQUIREMNT YOU CAN FEATCH THE REUIRED RECORDS BY USING ABOVE QUERY.
+
             var result = (from lstcustmer in _northwindContext.Customers select lstcustmer).Take(5).Skip(4);
             //It converts your data to jsonformat
             var convertedData = JsonConvert.SerializeObject(result);
@@ -130,18 +139,18 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
         public async Task<IActionResult> AgeWithFilter()
         {
             //example with dummydata
-            List<StudentData> lststudentsObj = new List<StudentData>()
-            {//List with multiple objects declaring and assigning the data like this way
-               new StudentData() { StudentID = 1, StudentName = "John", Age = 13} ,//this is one object
-               new StudentData() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,//this is one object
-               new StudentData() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,//this is one object
-               new StudentData() { StudentID = 4, StudentName = "Ram" , Age = 20} ,//this is one object
-               new StudentData() { StudentID = 5, StudentName = "Ron" , Age = 15 }//this is one object
-            };
+            List<StudentData> lststudentsObj = new List<StudentData>()//CREATE LIST OBJECT FIRST
+{//List with multiple objects declaring and assigning the data like this way
+   new StudentData() { StudentID = 1, StudentName = "John", Age = 13} ,//this is one object
+   new StudentData() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,//this is one object
+   new StudentData() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,//this is one object
+   new StudentData() { StudentID = 4, StudentName = "Ram" , Age = 20} ,//this is one object
+   new StudentData() { StudentID = 5, StudentName = "Ron" , Age = 15 }//this is one object
+};
             var filteredResult = from s in lststudentsObj
                                  where s.Age > 15 && s.Age <= 20
                                  select new { FullName = s.StudentName };//giving the alisaname
-                                                                         //It converts your data to jsonformat
+                                                                         //It converts your data to jsonstringformat
             var convertedData = JsonConvert.SerializeObject(filteredResult);
             return StatusCode(StatusCodes.Status200OK, convertedData);
 
@@ -152,17 +161,17 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
         {
             //example with dummydata
             List<StudentData> lststudentsObj = new List<StudentData>()
-            {
-               new StudentData() { StudentID = 1, StudentName = "John", Age = 13} ,
-               new StudentData() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
-               new StudentData() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
-               new StudentData() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
-               new StudentData() { StudentID = 5, StudentName = "Ron" , Age = 15 }
-            };
+{
+   new StudentData() { StudentID = 1, StudentName = "John", Age = 13} ,
+   new StudentData() { StudentID = 2, StudentName = "Moin",  Age = 21 } ,
+   new StudentData() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+   new StudentData() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+   new StudentData() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+};
             var orderByAscendingResult = from s in lststudentsObj
                                          orderby s.StudentName ascending
-                                         select s;
-
+                                         select s;//it will Show the  total columns 
+                           //Select new{StudentId,Age}//you can also select required columns
             var orderByDescendingResult = from s in lststudentsObj
                                           orderby s.StudentName descending
                                           select s;
@@ -172,18 +181,19 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
 
         }
         [HttpGet]
-        [Route("GroupByusage")]
+        [Route("GroupByusageWith dummydata")]
         public async Task<IActionResult> GroupByusage()
         {
             //example with dummydata
             List<StudentData> lststudentsObj = new List<StudentData>()
-            {
-               new StudentData() { StudentID = 1, StudentName = "John", Age = 13} ,
-               new StudentData() { StudentID = 2, StudentName = "Moin",  Age = 13 } ,
-               new StudentData() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
-               new StudentData() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
-               new StudentData() { StudentID = 5, StudentName = "Ron" , Age = 15 }
-            };
+{
+   new StudentData() { StudentID = 1, StudentName = "John", Age = 13} ,
+   new StudentData() { StudentID = 2, StudentName = "Moin",  Age = 13 } ,
+   new StudentData() { StudentID = 3, StudentName = "Bill",  Age = 18 } ,
+   new StudentData() { StudentID = 4, StudentName = "Ram" , Age = 20} ,
+   new StudentData() { StudentID = 5, StudentName = "Ron" , Age = 15 }
+};
+            //Sql Groupby Query:select   CompanyName,Count(*)     from Customers group by CompanyName
             var groupedStudents = lststudentsObj.GroupBy(s => s.Age)
                                      .Select(g => new { Age = g.Key, Students = g.ToList() });
             //It converts your data to jsonformat
@@ -198,9 +208,9 @@ namespace LinqInDepthQueriesForFreshExperienced.Controllers
             //example with dummydata
             // Define a list of fruits
             List<string> fruits = new List<string>
-        {
-            "apple", "banana", "orange", "apple", "grape", "banana", "apple"
-        };
+{
+"apple", "banana", "orange", "apple", "grape", "banana", "apple"
+};
 
             // Group the fruits using Query syntax(RealTime Usethis one)
             var groupedFruits = fruits.GroupBy(f => f)
